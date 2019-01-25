6af5268d003b63eeb19d06cf3f2cd246f98031d9
Alice Liang <alicel_@alices-macbook-air.local>
2019-01-24 21:04:48 -0500

    week two class exercise completed

--- Week Two/ViewController.swift
+++ Week Two/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print ("The view loaded!")
     }
 
-
+    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+        print ("The show message button was pressed!")
+        messageLabel.text = "You are great!"
+    }
+    
 }
 
